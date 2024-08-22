349193106e23bc1ab874835f00512c236174e231
KrishnaR35 <krishnaverma.0227@gmail.com>
2024-08-23 00:56:06 +0530

    Added IIFE

---
+++ 7_Functions/6_IIFE.js
@@ -0,0 +1,39 @@
+//  IIFE [Immediately Invoked Function Expression] -->
+
+/*
+    What is IIFE ?
+
+    An IIFE (Immediately Invoked Function Expression) is a JavaScript function
+    that runs as soon as it is defined. The primary purpose of an IIFE is to
+    create a new scope to avoid polluting the global namespace, which can help
+    prevent variables from conflicting with other scripts
+
+    Syntax of an IIFE -->
+
+    An IIFE is usually written by wrapping the function in parentheses and
+    immediately invoking it with another set of parentheses
+
+    (function()
+    {
+        Code here runs immediately
+        console.log('This is an IIFE')
+    })();
+
+    IIFE with Arrow -->
+
+    (() =>
+    {
+        console.log('This is an IIFE with an arrow function')
+    })();
+
+    ~ Semicolons are used in end of the IIFE to end the context
+*/
+
+console.clear()
+console.log("IIFE -->")
+console.log()
+
+(() =>
+{
+    console.log('This is an IIFE !')
+})();
